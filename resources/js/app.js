4a0f1c444f83ee3d1a936851034688fca1ce38cd
Christian Deacon <gamemann@gflclan.com>
2022-10-26 13:35:41 -0500

    Fix original button link.

--- resources/js/app.js
+++ resources/js/app.js
@@ -70,7 +70,7 @@ $(document).ready(function ()
             {
                 targets: 12,
                 "render": function ( data, type, row, meta ) {
-                    return '<div class="card-buttons text-center"><a href="' + row[8] + '" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Original</a></div>';
+                    return '<div class="card-buttons text-center"><a href="' + row[9] + '" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Original</a></div>';
                   }
             }
         ]
