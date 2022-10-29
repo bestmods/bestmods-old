db6b1a7320174fae5e030a06e3c363794624f979
Christian Deacon <gamemann@gflclan.com>
2022-10-29 12:49:01 -0500

    Make the URL unique.

---
+++ database/migrations/2022_10_29_174751_add_url_unique.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('mods', function (Blueprint $table) {
+            $table->unique('url');
+        });
+    }
+
+
+    public function down()
+    {
+    }
+};
