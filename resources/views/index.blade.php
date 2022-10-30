fdba4b637aa032d88476092ddfc37dde4ac89779
Christian Deacon <gamemann@gflclan.com>
2022-10-30 17:39:01 -0500

    Make header text a bit more longer.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <div class="text-white m-7">
-    <h1 class="text-3xl font-bold mb-4">Start Discovering!</h1>
+    <h1 class="text-3xl font-bold mb-4">Start Discovering Now!</h1>
     <table id="mods" class="card">
         <thead>
             <th>ID</th>
