e16754a320822994fa790adb5f495b1b46861cff
Christian Deacon <gamemann@gflclan.com>
2022-12-07 20:29:40 -0600

    Add content security policy header.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,4 @@
 <div class="text-white m-7 container mx-auto px-10">
     <h1 class="text-3xl text-center font-bold mb-4">Start Discovering Now!</h1>
-    <table id="mods" class="card" style="width: 100%;">
-        <thead>
-            <th>ID</th>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Game Name</th>
-            <th>Seed Name</th>
-            <th>Rating</th>
-            <th>Total Downloads</th>
-            <th>Total Views</th>
-            <th>URL</th>
-            <th>Seed URL</th>
-            <th>Custom URL</th>
-            <th>Buttons</th>
-            <th>Game Image</th>
-            <th>Seed Image</th>
-            <th>Seed Protocol</th>
-            <th>Seed Classes</th>
-            <th>Game Classes</th>
-        </thead>
-    </table>
+    <div id="content"></div>
 </div>
