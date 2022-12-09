59aa55cdc687bb845739439a3b21272ce11a20ca
Christian Deacon <gamemann@gflclan.com>
2022-12-09 09:39:46 -0600

    Use correct proxy gateway.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = ['192.168.112.0/20'];
+    protected $proxies = ['172.24.0.1'];
 
     /**
      * The headers that should be used to detect proxies.
